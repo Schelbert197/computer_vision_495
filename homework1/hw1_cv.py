f9791b7e5a4f5e744eb0448588655aa276528c46
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2024-04-05 16:49:52 -0500

    completed segmentation algorithm

--- homework1/hw1_cv.py
+++ homework1/hw1_cv.py
@@ -2,12 +2,11 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-import math
 from PIL import Image
 
 
 # Open the .bmp file
-image = Image.open("homework1/test.bmp")
+image = Image.open("homework1/gun.bmp")
 
 # Display basic information about the image
 print("Image format:", image.format)
@@ -21,27 +20,57 @@ image.show()
 
 
 def set_equivalence(val1, val2):
-    if equivalences:
-        for i in range(len(equivalences)):
-            if val1 in equivalences[i] and val2 in equivalences[i]:
-                pass
-            elif val1 in equivalences[i]:
-                equivalences[i].add(val2)
-            elif val2 in equivalences[i]:
-                equivalences[i].add(val1)
-            else:
-                equivalences.append({val1, val2})
-    else:
+    """Sets equivalences for disputed pixels"""
+    updated_equiv = False
+    for i in range(len(equivalences)):
+        if val1 in equivalences[i] and val2 in equivalences[i]:
+            updated_equiv = True
+            break
+        elif val1 in equivalences[i]:
+            equivalences[i].add(val2)
+            updated_equiv = True
+            break
+        elif val2 in equivalences[i]:
+            equivalences[i].add(val1)
+            updated_equiv = True
+            break
+
+    if not updated_equiv:
         equivalences.append({val1, val2})
-    # print(equivalences)
+
+
+def join_sets_with_shared_value(sets):
+    """Join sets that share a value."""
+    # Create a list to store the updated sets
+    updated_sets = []
+
+    # Iterate over each set in the list
+    for current_set in sets:
+        # Check if any existing set shares a value with the current set
+        joined = False
+        for updated_set in updated_sets:
+            if updated_set.intersection(current_set):
+                # If there's a shared value, merge the sets and mark as joined
+                updated_set |= current_set
+                joined = True
+                break
+
+        # If the current set didn't share a value with any existing set,
+        # add it to the updated sets list
+        if not joined:
+            updated_sets.append(current_set)
+
+    return updated_sets
 
 
 def correct_labels(label_mat, eq_list):
+    """Corrects the labels to only show the min accounting for equivalences"""
     for i in range(label_mat.shape[0]):
         for j in range(label_mat.shape[1]):
             for k in range(len(eq_list)):
                 if label_mat[i][j] in eq_list[k]:
-                    label_mat[i][j] = min(eq_list[k])
+                    label_mat[i][j] = max(eq_list[k])
+    return label_mat
 
 
 def CCL(image):
@@ -88,7 +117,9 @@ def CCL(image):
 
 
 new_img = CCL(image)
-new_img2 = new_img.T[::-1]
+updated_equiv = join_sets_with_shared_value(equivalences)
+new_img3 = correct_labels(new_img, updated_equiv)
+new_img2 = new_img3.T[::-1]
 # Plot the array as an image
 plt.imshow(new_img2, cmap='viridis', origin='lower')
 plt.colorbar()
