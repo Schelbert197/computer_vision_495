48c0113cdd09453413b1b0548987755a53da4a89
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2024-04-05 17:19:14 -0500

    added structuring element. Working on erosion

--- homework2/hw2_cv.py
+++ homework2/hw2_cv.py
@@ -19,7 +19,7 @@ print("Image size:", image.size)
 image.show()
 
 
-def dilate(image):
+def dilate(image, SE=(1, 2, 1, 2)):
     """Dilates an image"""
     new_image = np.zeros([image.size[0], image.size[1]])
     # 1 check each pixel for foreground
@@ -28,15 +28,15 @@ def dilate(image):
             if image.getpixel((u, v)) == 0:
                 pass
             else:
-                # 2 add all surrounding pixels to fg if pixel is in fg and in image
-                new_image = dilate_new_image(new_image, u, v)
+                # 2 add all surrounding pixels to fg if pixel in fg and image
+                new_image = dilate_new_image(new_image, u, v, SE)
     return new_image
 
 
-def dilate_new_image(new_image, u, v):
+def dilate_new_image(new_image, u, v, SE=(1, 2, 1, 2)):
     """modifies the new image 2-D array to add dilation"""
-    for i in range(u - 1, u + 2):
-        for j in range(v - 1, v + 2):
+    for i in range(u - SE[0], u + SE[1]):
+        for j in range(v - SE[2], v + SE[3]):
             if (0 <= i < new_image.shape[0]) and (0 <= j < new_image.shape[1]):
                 # make 1
                 new_image[i][j] = 1
@@ -45,9 +45,70 @@ def dilate_new_image(new_image, u, v):
     return new_image
 
 
-new_img = dilate(image)
+def erode(image, SE=(1, 2, 1, 2)):
+    """Erodes an image"""
+    new_image = np.zeros([image.size[0], image.size[1]])
+    # 1 check each pixel for foreground
+    for u in range(image.size[0]):
+        for v in range(image.size[1]):
+            if image.getpixel((u, v)) == 0:
+                pass
+            else:
+                # 2 add all surrounding pixels to fg if pixel in fg and image
+                new_image = erode_new_image(new_image, u, v, SE)
+    return new_image
+
+
+def erode_new_image(new_image, u, v, SE=(1, 2, 1, 2)):
+    """modifies the new image 2-D array to add dilation"""
+    for i in range(u - SE[0], u + SE[1]):
+        for j in range(v - SE[2], v + SE[3]):
+            if (0 <= i < new_image.shape[0]) and (0 <= j < new_image.shape[1]):
+                # make 1
+                new_image[i][j] = 1
+            else:
+                pass
+    return new_image
+
+
+new_img = dilate(image, SE=(2, 2, 2, 2))
 new_img2 = new_img.T[::-1]
 # Plot the array as an image
 plt.imshow(new_img2, cmap='viridis', origin='lower')
 plt.colorbar()
 plt.show()
+# def superimpose_arrays(large_array, small_array, index):
+#     """
+#     Superimpose the values of the small array over the larger array at the specified index.
+#     """
+#     # Get the dimensions of the small array
+#     small_shape = small_array.shape
+
+#     # Get the indices to replace in the larger array
+#     start_index = (index[0] - small_shape[0] // 2,
+#                    index[1] - small_shape[1] // 2)
+#     end_index = (start_index[0] + small_shape[0],
+#                  start_index[1] + small_shape[1])
+
+#     # Replace the values in the larger array with the values of the smaller array
+#     large_array[start_index[0]:end_index[0],
+#                 start_index[1]:end_index[1]] = small_array
+
+#     return large_array
+
+
+# # Example usage:
+# # Create large and small arrays
+# large_array = np.zeros((10, 10))
+# small_array = np.array([[1, 1, 1],
+#                         [1, 0, 1],
+#                         [1, 1, 1]])
+
+# # Index at which to superimpose the small array
+# index = (9, 5)
+
+# # Superimpose the small array over the larger array
+# result_array = superimpose_arrays(large_array, small_array, index)
+
+# print("Result array:")
+# print(result_array)
