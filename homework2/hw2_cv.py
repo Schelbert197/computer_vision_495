4d72676c2720dc5b5e07d85eca04ed8e4e256a91
Srikanth Schelbert <srikanthschelbert2024@u.northwestern.edu>
2024-04-11 17:23:01 -0500

    very close to boundary, need to fix shape issue

--- homework2/hw2_cv.py
+++ homework2/hw2_cv.py
@@ -15,9 +15,6 @@ print("Image format:", image.format)
 print("Image mode:", image.mode)
 print("Image size:", image.size)
 
-# Show the image
-image.show()
-
 
 def dilate(image, SE=(1, 2, 1, 2)):
     """Dilates an image"""
@@ -55,60 +52,120 @@ def erode(image, SE=(1, 2, 1, 2)):
                 pass
             else:
                 # 2 add all surrounding pixels to fg if pixel in fg and image
-                new_image = erode_new_image(new_image, u, v, SE)
+                new_image = erode_new_image(new_image, u, v, image, SE)
     return new_image
 
 
-def erode_new_image(new_image, u, v, SE=(1, 2, 1, 2)):
+def erode_new_image(new_image, u, v, image, SE=(1, 2, 1, 2)):
     """modifies the new image 2-D array to add dilation"""
+
+    set_one = True
     for i in range(u - SE[0], u + SE[1]):
         for j in range(v - SE[2], v + SE[3]):
             if (0 <= i < new_image.shape[0]) and (0 <= j < new_image.shape[1]):
-                # make 1
-                new_image[i][j] = 1
+                # set u,v pixel to 0 if it doesn't match image to SE
+                if int(image.getpixel((i, j))) >= 1:
+                    pass
+                else:
+                    # keeps u,v val 0 if not a match
+                    set_one = False
+
             else:
                 pass
+
+    # Sets u,v value to 1 if it goes through the whole loop and never breaks
+        # no just only set to 1 if loops complete otherwise it is already 0
+    if set_one == True:
+        new_image[u][v] = 1
     return new_image
 
 
-new_img = dilate(image, SE=(2, 2, 2, 2))
-new_img2 = new_img.T[::-1]
-# Plot the array as an image
-plt.imshow(new_img2, cmap='viridis', origin='lower')
-plt.colorbar()
-plt.show()
-# def superimpose_arrays(large_array, small_array, index):
-#     """
-#     Superimpose the values of the small array over the larger array at the specified index.
-#     """
-#     # Get the dimensions of the small array
-#     small_shape = small_array.shape
-
-#     # Get the indices to replace in the larger array
-#     start_index = (index[0] - small_shape[0] // 2,
-#                    index[1] - small_shape[1] // 2)
-#     end_index = (start_index[0] + small_shape[0],
-#                  start_index[1] + small_shape[1])
-
-#     # Replace the values in the larger array with the values of the smaller array
-#     large_array[start_index[0]:end_index[0],
-#                 start_index[1]:end_index[1]] = small_array
-
-#     return large_array
+def convert_to_image(array):
+    """Converts a numpy array to an image"""
+    return Image.fromarray(array * 255)
 
 
-# # Example usage:
-# # Create large and small arrays
-# large_array = np.zeros((10, 10))
-# small_array = np.array([[1, 1, 1],
-#                         [1, 0, 1],
-#                         [1, 1, 1]])
+def closing(image, input_SE=(2, 2, 2, 2)):
+    "closes holes in an image"
+    new_img = dilate(image, SE=input_SE)
+    new_img2 = erode(convert_to_image(new_img), SE=input_SE)
+    new_img3 = convert_to_image(new_img2)
+    new_img3.show("Closed Image")
 
-# # Index at which to superimpose the small array
-# index = (9, 5)
 
-# # Superimpose the small array over the larger array
-# result_array = superimpose_arrays(large_array, small_array, index)
+def opening(image, input_SE=(2, 2, 2, 2)):
+    """Opens gaps within an image"""
+    new_img = erode(image, SE=input_SE)
+    new_img2 = dilate(convert_to_image(new_img), SE=input_SE)
+    new_img3 = convert_to_image(new_img2)
+    new_img3.show("Opened Image")
 
-# print("Result array:")
-# print(result_array)
+
+def boundary(image, input_SE=(2, 2, 2, 2)):
+    """Finds the boundary of the image"""
+    # create first closed image
+    new_img = dilate(image, SE=input_SE)
+    new_img2 = erode(convert_to_image(new_img), SE=input_SE)
+    # new_img2 = new_img2.T[::-1]
+    print(new_img2.shape)
+    plt.imshow(new_img2, cmap='viridis', origin='lower')
+    plt.colorbar()
+    plt.show()
+
+    # Create new dilated image with closed array
+    new_img6 = dilate(convert_to_image(new_img2), SE=(1, 1, 1, 1))
+    new_img7 = new_img6.T[:-1:]
+    new_img7 = new_img7.resize(
+        (new_img2.shape[1], new_img2.shape[0]))  # Resize to match shape
+    print(new_img7.shape)
+    plt.imshow(new_img7, cmap='viridis', origin='lower')
+    plt.colorbar()
+    plt.show()
+
+    # subtract images and show
+    # new_img4 = convert_to_image(new_img7 - new_img2)
+    # new_img4.show()
+
+# new_img = dilate(image, SE=(2, 2, 2, 2))
+# # new_img2 = new_img.T[::-1]
+# new_img2 = erode(convert_to_image(new_img), SE=(2, 2, 2, 2))
+# new_img3 = convert_to_image(new_img2)
+# new_img3.show()
+# new_img4 = new_img3.T[::-1]
+# Plot the array as an imagebreak_outer
+# plt.imshow(new_img3, cmap='viridis', origin='lower')
+# plt.colorbar()
+# plt.show()
+
+
+if __name__ == "__main__":
+    response = input(
+        "\nWhat would you like to do? \nErode: e\nDilate: d\nClose: c\nOpen: o\nBoundary: b\n(Type Answer):")
+    match response:
+        case 'd':
+            # Show the image
+            image.show("Original Image")
+            new_img = dilate(image, SE=(2, 2, 2, 2))
+            new_img2 = new_img.T[::-1]
+            plt.imshow(new_img2, cmap='viridis', origin='lower')
+            plt.colorbar()
+            plt.show()
+        case 'e':
+            # Show the image
+            image.show("Original Image")
+            new_img = erode(image, SE=(1, 1, 1, 1))
+            new_img2 = new_img.T[::-1]
+            plt.imshow(new_img2, cmap='viridis', origin='lower')
+            plt.colorbar()
+            plt.show()
+        case 'c':
+            # Show the image
+            image.show("Original Image")
+            closing(image, (2, 2, 2, 2))
+        case 'o':
+            # Show the image
+            image.show("Original Image")
+            opening(image, (1, 1, 1, 1))
+        case 'b':
+            image.show()
+            boundary(image)
